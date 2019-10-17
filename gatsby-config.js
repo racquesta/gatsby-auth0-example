a3851583c437d86ddd6f6e4c09a89b6cee5d7d41
racquesta <racquesta@trilogyed.com>
2019-10-17 11:13:32 -0400

    messing with options

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify",
       options: {
-        generateMatchPathRewrites: false,
+        //generateMatchPathRewrites: true,
       },
     },
   ],
